c481841c887c07676358da58b33cf0eb4770d4f5
William Machado <williamlm.machado@gmail.com>
2021-05-25 09:07:24 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,8 +1,10 @@
 package com.nelioalves.cursomc.services;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
@@ -22,4 +24,9 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
 }
